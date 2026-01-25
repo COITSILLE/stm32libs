dfa098ada8cd1005ea974f8c77da6e813e06d114
COITSILLE <losith22222@gmail.com>
2026-01-25 15:17:54 +0800

    Enhance key handling with edge detection and callbacks
    Refactor key processing and initialization to support edge detection and callback functionality.

--- key/Src/key.c
+++ key/Src/key.c
@@ -2,52 +2,61 @@
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
 
-void Key_Init(Key_Typedef *key, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
+#define DEBOUNCE_DURATION 20U
+
+void Key_Init(Key_Typedef *key, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, 
+            GPIO_PinState valid_pin_state, KeyEdgeType edge_type, void (*KeyCallback)(void)){
     key->GPIOx = GPIOx;
     key->GPIO_Pin = GPIO_Pin;
+    key->valid_pin_state = valid_pin_state;
+    key->edge_type = edge_type;
+    key->KeyCallback = KeyCallback;
     key->state = KEY_RELEASED;
     key->debounce_time = 0;
 }
-void Key_Proc(Key_Typedef *key, KeyCallback callback){
+void Key_Proc(Key_Typedef *key){
+    
     GPIO_PinState state = HAL_GPIO_ReadPin(key->GPIOx, key->GPIO_Pin);
-    switch (state) {
-        case GPIO_PIN_SET:
-            switch (key->state) {
-                case KEY_RELEASED:
-                    key->state = KEY_DETECT;
-                    key->debounce_time = HAL_GetTick();
-                    break;
-                case KEY_DETECT:
-                    if (HAL_GetTick() - key->debounce_time > 20) {
-                        key->state = KEY_PRESSED;
-                    }
-                    if (callback != NULL) callback();
-                    break;
-                case KEY_PRESSED:
+    if (state == key->valid_pin_state) {
+        switch (key->state) {
+            case KEY_WAITRELEASED:
+                break;
+            case KEY_RELEASED:
+                key->state = KEY_PDETECT;
+                key->debounce_time = HAL_GetTick();
+                break;
+            case KEY_PDETECT:
+                if (HAL_GetTick() - key->debounce_time > DEBOUNCE_DURATION) {
                     key->state = KEY_WAITRELEASED;
-                    break;
-                case KEY_WAITRELEASED:
-                    break;
-                default:
-                    break;
+                    if (key->KeyCallback != NULL && (key->edge_type == RISING_EDGE || key->edge_type == BOTH_EDGE)) key->KeyCallback();
+                }
+                break;
+            case KEY_RDETECT:
+                key->state = KEY_WAITRELEASED;
+                break;
+            default:
+                break;
             }
-            break;
-        case GPIO_PIN_RESET:
-            switch (key->state) {
-                case KEY_RELEASED:
-                        break;
-                case KEY_DETECT:
-                        key->state = KEY_RELEASED;
-                     break;
-                    case KEY_PRESSED:
+    }
+    else{
+        switch (key->state) {
+            case KEY_RELEASED:
+                break;
+            case KEY_WAITRELEASED:
+                key->state = KEY_RDETECT;
+                key->debounce_time = HAL_GetTick();
+                break;
+            case KEY_RDETECT:
+                if (HAL_GetTick() - key->debounce_time > DEBOUNCE_DURATION) {
                     key->state = KEY_RELEASED;
-                        break;
-                case KEY_WAITRELEASED:
-                        key->state = KEY_RELEASED;
-                        break;
-                 default:
-                       break;
+                    if (key->KeyCallback != NULL && (key->edge_type == FALLING_EDGE || key->edge_type == BOTH_EDGE)) key->KeyCallback();
+                }
+                break;
+            case KEY_PDETECT:
+                key->state = KEY_RELEASED;
+                break;
+            default:
+                break;
             }
-            break;
     }
 }
